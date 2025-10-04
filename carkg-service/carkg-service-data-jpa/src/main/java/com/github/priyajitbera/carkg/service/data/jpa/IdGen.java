05bfdec5d6bc2ac139185156f8c07fba2ef7835a
Priyajit Bera <priyajitbera@gmail.com>
2025-10-05 00:21:52 +0530

    Add IdGen

---
+++ carkg-service/carkg-service-data-jpa/src/main/java/com/github/priyajitbera/carkg/service/data/jpa/IdGen.java
@@ -0,0 +1,12 @@
+package com.github.priyajitbera.carkg.service.data.jpa;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class IdGen {
+
+    public String generate(String... parentIdChain) {
+        return Arrays.stream(parentIdChain).map(String::toLowerCase).map(i -> i.replace(" ", "_"))
+                .collect(Collectors.joining("-"));
+    }
+}
