9331904b32e812b0e6d98dab34e78b70786ec0a8
Priyajit Bera <priyajitbera@gmail.com>
2025-10-12 17:49:10 +0530

    Add embedding with Gemini

--- carkg-service/carkg-service-api/src/main/java/com/github/priyajitbera/carkg/service/api/service/EmbeddingService.java
+++ carkg-service/carkg-service-api/src/main/java/com/github/priyajitbera/carkg/service/api/service/EmbeddingService.java
@@ -1,10 +1,12 @@
 package com.github.priyajitbera.carkg.service.api.service;
 
+import com.github.priyajitbera.carkg.service.api.client.GeminiClient;
 import org.springframework.ai.embedding.Embedding;
 import org.springframework.ai.embedding.EmbeddingRequest;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.ollama.OllamaEmbeddingModel;
 import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,17 +14,31 @@ import java.util.List;
 @Service
 public class EmbeddingService {
 
-    private final OllamaEmbeddingModel embeddingModel;
+    private final String GEMINI = "GEMINI";
 
-    public EmbeddingService(OllamaEmbeddingModel embeddingModel) {
+    private final String EMBEDDING_STRATEGY;
+    private final OllamaEmbeddingModel embeddingModel;
+    private final GeminiClient geminiClient;
+
+    public EmbeddingService(
+            @Value("${embedding.strategy}")
+            String embeddingStrategy,
+            OllamaEmbeddingModel embeddingModel, GeminiClient geminiClient) {
+        this.EMBEDDING_STRATEGY = embeddingStrategy;
         this.embeddingModel = embeddingModel;
+        this.geminiClient = geminiClient;
     }
 
     public Embedding embedding(String text) {
-        EmbeddingResponse embeddingResponse = embeddingModel.call(
-                new EmbeddingRequest(
-                        List.of(text),
-                        OllamaOptions.builder().model("nomic-embed-text").truncate(false).build()));
-        return embeddingResponse.getResults().get(0);
+        if (GEMINI.equals(EMBEDDING_STRATEGY)) {
+            float[] vector = geminiClient.embed(text);
+            return new Embedding(vector, -1);
+        } else {
+            EmbeddingResponse embeddingResponse = embeddingModel.call(
+                    new EmbeddingRequest(
+                            List.of(text),
+                            OllamaOptions.builder().model("nomic-embed-text").truncate(false).build()));
+            return embeddingResponse.getResults().get(0);
+        }
     }
 }
