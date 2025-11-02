fad15ed657e7faa6c35a044e24971eecefccdf80
Priyajit Bera <priyajitbera@gmail.com>
2025-11-03 00:31:09 +0530

    Update component scan packages

--- carkg-service/carkg-service-mcp-server/src/main/java/com/github/priyajitbera/carkg/service/mcp/CarKgMcpApplication.java
+++ carkg-service/carkg-service-mcp-server/src/main/java/com/github/priyajitbera/carkg/service/mcp/CarKgMcpApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.github.priyajitbera.carkg.service.common"})
+@ComponentScan(basePackages = {
+        "com.github.priyajitbera.carkg.service.common",
+        "com.github.priyajitbera.carkg.service.mcp"})
 @SpringBootApplication
 public class CarKgMcpApplication {
 
