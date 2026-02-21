151028d6589b1dbc2a5805b604b7893f32684933
Priyajit Bera <priyajitbera@gmail.com>
2026-02-21 21:08:37 +0530

    generate and log request id, log duration

--- carkg-service/carkg-service-common/src/main/java/com/github/priyajitbera/carkg/service/common/RequestLoggingFilter.java
+++ carkg-service/carkg-service-common/src/main/java/com/github/priyajitbera/carkg/service/common/RequestLoggingFilter.java
@@ -5,27 +5,61 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
 @Component
 @Slf4j
 public class RequestLoggingFilter extends OncePerRequestFilter {
+
+  public static final String REQUEST_ID_KEY = "req-id";
+
   @Override
   protected void doFilterInternal(
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
+    StopWatch stopWatch = new StopWatch();
+    stopWatch.start();
+    request.setAttribute(REQUEST_ID_KEY, newRequestId());
     ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
-    log.info("{} {}", request.getMethod(), request.getRequestURL().toString());
+    log.info("{} {} {}", requestIdLog(), request.getMethod(), request.getRequestURL().toString());
     filterChain.doFilter(requestWrapper, response);
     log.info(
-        "{} {} {}",
+        "{} {} {} {}",
+        requestIdLog(),
         request.getMethod(),
         request.getRequestURL().toString(),
         requestWrapper.getContentAsString());
+    stopWatch.stop();
     log.info(
-        "{} {} {}", request.getMethod(), request.getRequestURL().toString(), response.getStatus());
+        "{} {} {} {} duration: {} ms",
+        requestIdLog(),
+        request.getMethod(),
+        request.getRequestURL().toString(),
+        response.getStatus(),
+        stopWatch.getTotalTimeMillis());
+  }
+
+  public static String requestIdLog() {
+    return REQUEST_ID_KEY
+        + ": "
+        + RequestContextHolder.currentRequestAttributes()
+            .getAttribute(REQUEST_ID_KEY, RequestAttributes.SCOPE_REQUEST);
+  }
+
+  public static String newRequestId() {
+    return ZonedDateTime.now(ZoneId.of("UTC"))
+            .format(DateTimeFormatter.ofPattern("yyyyMMddssHHmmss"))
+        + "-"
+        + UUID.randomUUID().toString().substring(0, 6);
   }
 }
