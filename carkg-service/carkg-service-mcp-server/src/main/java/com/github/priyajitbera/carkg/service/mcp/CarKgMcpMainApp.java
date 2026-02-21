83c207e2efba0a3512bd7a9518b69c95b43642a4
Priyajit Bera <priyajitbera@gmail.com>
2026-02-21 21:49:15 +0530

    rename to CarKgMcpMainApp

--- carkg-service/carkg-service-mcp-server/src/main/java/com/github/priyajitbera/carkg/service/mcp/CarKgMcpMainApp.java
+++ carkg-service/carkg-service-mcp-server/src/main/java/com/github/priyajitbera/carkg/service/mcp/CarKgMcpMainApp.java
@@ -10,9 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
       "com.github.priyajitbera.carkg.service.mcp"
     })
 @SpringBootApplication
-public class CarKgMcpApplication {
+public class CarKgMcpMainApp {
 
   public static void main(String[] args) {
-    SpringApplication.run(CarKgMcpApplication.class, args);
+    SpringApplication.run(CarKgMcpMainApp.class, args);
   }
 }
